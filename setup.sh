c084e2d1c5ac1ae82c3dbebed20f37b1fb3dee90
Mireu / 미르 <58178547+Mireu-Labcon@users.noreply.github.com>
2021-05-13 02:33:15 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,6 +23,11 @@ sudo dpkg --add-architecture i386
 sudo add-apt-repository ppa:mikhailnov/pulseeffects
 sudo apt install pulseaudio pulseeffects --install-recommends
 
+wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
+sudo wget http://deb.playonlinux.com/playonlinux_precise.list -O /etc/apt/sources.list.d/playonlinux.list
+sudo apt-get -y update
+sudo apt-get -y install playonlinux
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y autoremove
