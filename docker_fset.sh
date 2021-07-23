4ba554898bf38caeec126cbb67e0bf756d9efdbb
mireu-labcon <alfmalfm1214@gmail.com>
2021-07-24 03:38:51 +0900

    docker build set code add

---
+++ docker_fset.sh
@@ -0,0 +1,10 @@
+yum -y install python3
+yum -y install python3-pip
+
+yum -y install sudo
+yum -y install vim
+yum -y install unzip
+yum -y install nano
+yum -y install htop
+yum -y install wget
+yum -y install net-tools
---
+++ docker_set.sh
@@ -0,0 +1,13 @@
+apt-get -y install python3
+apt-get -y install python3-pip
+
+apt-get -y install sudo
+apt-get -y install vim
+apt-get -y install unzip
+apt-get -y install nano
+apt-get -y install htop
+apt-get -y install wget
+apt-get -y install net-tools
+
+git config --global user.email "alfmalfm1214@gmail.com"
+git config --global user.name "mireu-labcon"
