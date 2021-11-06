bd4d1e7389ee4fed4b2592c7d9087e7f74540bb2
Mireu / 미르 <58178547+Mireu-Labcon@users.noreply.github.com>
2021-11-07 03:50:42 +0900

    불필요한 프로그램 설치 제외 처리

--- setup.sh
+++ setup.sh
@@ -2,20 +2,10 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y autoremove
 
-sudo apt-get -y install sudo vim unzip nano htop wget net-tools ufw mc git
+sudo apt-get -y install unzip nano htop wget net-tools ufw mc git
 sudo apt-get -y install gnome-tweak-tool fonts-nanum fonts-nanum-extra fonts-nanum-coding fcitx-hangul
 
-sudo apt-get -y install python3-pip gcc
-
-sudo dpkg --add-architecture i386
-sudo add-apt-repository ppa:mikhailnov/pulseeffects
-sudo apt install pulseaudio pulseeffects --install-recommends
-
-sudo snap install code-insiders --classic
-sudo snap install beekeeper-studio
-sudo snap install space
-sudo snap install pycharm-community --classic
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y autoremove
+sudo apt-get -y install python3-pip 
+sudo apt-get -y install gcc
+sudo apt-get -y install golang
+sudo apt-get -y install nodejs npm
